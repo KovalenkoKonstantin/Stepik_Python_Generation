a2374605db99c32cec7fd75db9579bc3f99ed7d2
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2023-12-15 16:34:11 +0300

    Create 4.py

---
+++ 4/4.py
@@ -0,0 +1,49 @@
+# answer = input('Какой язык программирования мы изучаем?')
+# if answer == 'Python':
+#     print('Верно! Мы ботаем Python =)')
+#     print('Python - отличный язык!')
+# else:
+#     print('Не совсем так!')
+# num1 = int(input())
+# num2 = int(input())
+#
+# if num1 < num2:
+#     print(num1, 'меньше чем', num2)
+# if num1 > num2:
+#     print(num1, 'больше чем', num2)
+# if num1 == num2:                      # используем двойное равенство
+#     print(num1, 'равно', num2)
+# if num1 != num2:
+#     print(num1, 'не равно', num2)
+
+# age = int(input())
+# if 3 <= age <= 6:
+#     print('Вы ребёнок')
+
+# if a == b == c:
+#     print('числа равны')
+# else:
+#     print('числа не равны')
+# num = int(input())
+# last_digit = num % 10    # последняя цифра числа
+# first_digit = num // 10  # первая цифра числа
+#
+# if last_digit == first_digit:
+#     print('ДА')
+# else:
+#     print('НЕТ')
+
+# num1, num2, num3 = int(input()), int(input()), int(input())
+# counter = 0  # переменная счётчик
+# if num1 % 2 == 0:
+#     counter = counter + 1  # увеличиваем счётчик на 1
+# if num2 % 2 == 0:
+#     counter = counter + 1  # увеличиваем счётчик на 1
+# if num3 % 2 == 0:
+#     counter = counter + 1  # увеличиваем счётчик на 1
+# print(counter)
+# i = int(input())
+# if i % 2 != 0:
+#     print(i, 'нечётное')
+# else:
+#     print(i, 'чётное')
